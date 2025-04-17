a9da2fdaa9849a987276cfc43442463caa7cd49e
Ralf <ralf@blackgame.net>
2025-04-17 08:12:56 +0200

    feat: add kacheln

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	let length = $state(0);
+
+	function resize() {
+		length = Math.trunc(window.innerWidth / 112) * Math.trunc(window.innerHeight / 112);
+	}
+
+	onMount(() => {
+		resize();
+
+		onresize = () => resize();
+	});
+</script>
+
+<main
+	class="flex h-screen max-h-screen min-h-screen w-full max-w-full min-w-full flex-wrap overflow-hidden"
+>
+	{#if browser}
+		{#each { length }, item}
+			<div class="m-auto h-28 w-28 border-2">
+				{item + 1}
+			</div>
+		{/each}
+	{/if}
+</main>

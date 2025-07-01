23cfe6c8066240aad0e91ea145ff6f19f025995b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-01 21:06:05 +0200

    feat(Taskleiste): add Taskleiste component for window management

---
+++ src/lib/desktop/components/Taskleiste.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { windows } from '../stores/windows.svelte';
+	import { minimizeWindow } from '../utils/window';
+	type Ausrichtung = 'links' | 'mitte' | 'rechts';
+	export let ausrichtung: Ausrichtung;
+</script>
+
+<nav
+	class="bg-base-300 fixed bottom-0 z-50 flex h-14 min-h-fit w-full p-2"
+	class:justify-start={ausrichtung === 'links'}
+	class:justify-center={ausrichtung === 'mitte'}
+	class:justify-end={ausrichtung === 'rechts'}
+>
+	{#each windows as win (win.id)}
+		<button class="btn btn-square" on:click={() => minimizeWindow(win.id)}>🗕</button>
+	{/each}
+</nav>

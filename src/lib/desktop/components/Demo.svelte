cb5b4da9a9919d3e88c4bacf1ec2597fbc5a3858
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-03 01:09:31 +0200

    feat(Demo): add demo dialog component for Blacky-OS

---
+++ src/lib/desktop/components/Demo.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	let { open = $bindable(true) }: { open: boolean } = $props();
+
+	function closeDialog() {
+		open = false;
+	}
+</script>
+
+<dialog id="demo" class="modal" {open}>
+	<div class="modal-box max-w-1/3 min-w-2xl">
+		<h3 class="text-lg font-bold">Blacky-OS Demo</h3>
+		<p class="py-4">
+			You are currently using an early demo version of <strong>Blacky-OS</strong>.<br />
+			The system is still under active development.<br />
+			Many features such as window management, and taskbar are already functional — but many more are
+			coming soon!<br />
+			<a
+				href="https://github.com/BlackTiger007/blacky-os"
+				class="link-primary link"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				GitHub Repository
+			</a>
+		</p>
+		<div class="modal-action">
+			<button class="btn" onclick={closeDialog}>Close Window</button>
+		</div>
+	</div>
+</dialog>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,10 @@
 <script lang="ts">
 	import MyWindow from '$lib/windows/Window.svelte';
 	import Taskleiste from '$lib/desktop/components/Taskleiste.svelte';
-	import Sandbox from '$lib/desktop/apps/Sandbox.svelte';
-	import { windowManager } from '$lib/windows/WindowManager.svelte';
 	import { windows } from '$lib/windows/windowStore.svelte';
+	import Demo from '$lib/desktop/components/Demo.svelte';
 
-	function openSandbox() {
-		windowManager.add({
-			title: 'test',
-			component: Sandbox,
-			position: { x: 5, y: 30 },
-			size: { width: 300, height: 900 },
-			minimized: false,
-			maximized: false,
-			fullscreen: false,
-			closable: true,
-			resizable: true,
-			movable: true,
-			visible: true,
-			zIndex: 0
-		});
-	}
+	let open = $state(true);
 </script>
 
 <div class="h-screen w-screen bg-cover bg-center">
@@ -28,9 +12,9 @@
 		<MyWindow {win} />
 	{/each}
 
+	<Demo bind:open />
+
 	<Taskleiste ausrichtung="links" showSeconds={false} />
 
-	<button class="btn btn-primary fixed top-40 right-4" on:click={openSandbox}>
-		Sandbox öffnen
-	</button>
+	<button class="btn btn-primary fixed top-40 right-4" onclick={() => (open = true)}>Infos</button>
 </div>

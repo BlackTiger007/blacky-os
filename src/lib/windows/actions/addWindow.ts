21fe7874ac5cc5176cced24f6cbbc38c5893986a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-02 21:19:33 +0200

    feat(window): implement addWindow function to create and add new windows

--- src/lib/windows/actions/addWindow.ts
+++ src/lib/windows/actions/addWindow.ts
@@ -1,5 +1,5 @@
-import { windows } from './windowStore.svelte';
-import type { MyWindow } from './types';
+import { windows } from '../windowStore.svelte';
+import type { MyWindow } from '../types';
 
 export function addWindow(params: Omit<MyWindow, 'id'> & Partial<Pick<MyWindow, 'id'>>) {
 	const windowWithId: MyWindow = {

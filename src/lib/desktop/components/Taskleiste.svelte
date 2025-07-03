377316dc6be338a971354fb710c1b62df9141a1d
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-07-03 22:16:52 +0200

    fix(Taskleiste): improve button styles and structure in navigation

--- src/lib/desktop/components/Taskleiste.svelte
+++ src/lib/desktop/components/Taskleiste.svelte
@@ -51,17 +51,19 @@
 	});
 </script>
 
-<nav class="bg-base-300 fixed bottom-0 z-[9900] flex h-14 w-full items-center justify-between p-2">
+<nav
+	class="bg-base-300 fixed bottom-0 z-[9900] flex h-14 w-full items-center justify-between gap-2 p-2"
+>
 	<div
-		class="flex flex-1"
+		class="flex flex-1 overflow-x-auto overflow-y-hidden"
 		class:justify-start={ausrichtung === 'links'}
 		class:justify-center={ausrichtung === 'mitte'}
 		class:justify-end={ausrichtung === 'rechts'}
 	>
-		<button class="btn" onclick={addNewWindow}>New Window</button>
+		<button class="btn btn-ghost" onclick={addNewWindow}>New Window</button>
 		{#each windows as win (win.id)}
 			<button
-				class="btn"
+				class="btn btn-ghost"
 				class:btn-active={win.aktiv}
 				onclick={() => {
 					if (win.aktiv || win.minimized) {
@@ -75,14 +77,16 @@
 		{/each}
 	</div>
 
-	<time class="grid text-right text-xs" datetime={date.toISOString()}>
-		<p>
-			{#if showSeconds}
-				{date.toLocaleTimeString()}
-			{:else}
-				{date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-			{/if}
-		</p>
-		<p>{date.toLocaleDateString([], { day: '2-digit', month: '2-digit', year: 'numeric' })}</p>
-	</time>
+	<button>
+		<time class="grid text-right text-xs" datetime={date.toISOString()}>
+			<p>
+				{#if showSeconds}
+					{date.toLocaleTimeString()}
+				{:else}
+					{date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+				{/if}
+			</p>
+			<p>{date.toLocaleDateString([], { day: '2-digit', month: '2-digit', year: 'numeric' })}</p>
+		</time>
+	</button>
 </nav>
